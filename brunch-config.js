5ba14a20ef4b357d1a03d259db63eceec5414bcd
Chris I Powell <chris@chrisipowell.co.uk>
2016-09-25 18:30:41 +0100

    Initial commit of generated brunch project

---
+++ brunch-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/vendor.js': /^(?!app)/,
+        'js/app.js': /^app/
+      }
+    },
+    stylesheets: {joinTo: 'style/chrisipowell.css'}
+  },
+
+  plugins: {
+    babel: {presets: ['es2015']}
+  }
+};
